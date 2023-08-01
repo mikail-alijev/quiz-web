770df4ac8df69dfb7718ab4fe6d4025faee48bef
Mikail Alijev <mikail.alijev@illuminetsolutions.com>
2023-08-01 14:04:29 +0100

    Change back-end url

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -1,8 +1,10 @@
-angular.module('app').factory('ConfigService', [function() {
-
+angular.module("app").factory("ConfigService", [
+  function () {
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase =
+      "https://quiz-api-ma-env.eba-3f3kyzft.eu-west-2.elasticbeanstalk.com/";
 
     return service;
-}]);
+  },
+]);
